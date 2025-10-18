2bd98b41dab9164c8a6b75a00343b0904c2d279f
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-19 00:14:18 +0530

    Initial Commit

---
+++ Arrays/ArrayRemoveTens.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class ArrayRemoveTens {
+    public static void main(String[] args) {
+        int[] inputData = { 1, 10, 10, 2 };
+
+        int[] filteredArray = withoutTen(inputData);
+
+        System.out.println("Array after removing 10's: " + Arrays.toString(filteredArray));
+    }
+
+    public static int[] withoutTen(int[] originalArray) {
+        int[] cleanedArray = new int[originalArray.length];
+        int position = 0;
+
+        for (int element : originalArray) {
+            if (element != 10) {
+                cleanedArray[position] = element;
+                position++;
+            }
+        }
+
+        return cleanedArray;
+    }
+}
