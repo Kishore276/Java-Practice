502bab543735cd3d44faad03817298a1f2ae02e4
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-19 00:16:03 +0530

    Initial Commit

---
+++ Arrays/CharacterCodeConverter.java
@@ -0,0 +1,10 @@
+public class CharacterCodeConverter {
+    public static void main(String[] args) {
+        int[] charCodes = { 65, 66, 67, 68, 101 };
+
+        System.out.print("Characters: ");
+        for (int code : charCodes) {
+            System.out.print((char) code + " ");
+        }
+    }
+}
