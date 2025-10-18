725d5623e494d152ffcd9665b8c9e7fc4a78d258
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-19 00:16:43 +0530

    Initial Commit

---
+++ Arrays/ElementSearchEngine.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ElementSearchEngine {
+    public static void main(String[] args) {
+        int[] dataList = { 1, 4, 34, 56, 7 };
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to search: ");
+        int targetValue = input.nextInt();
+
+        int foundPosition = -1;
+
+        for (int counter = 0; counter < dataList.length; counter++) {
+            if (dataList[counter] == targetValue) {
+                foundPosition = counter;
+                break;
+            }
+        }
+
+        if (foundPosition == -1) {
+            System.out.println(-1);
+        } else {
+            System.out.println("The number " + targetValue + " is found at index: " + foundPosition);
+        }
+
+        input.close();
+    }
+}
