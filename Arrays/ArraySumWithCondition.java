af8bf0fdd7aef2b59abf9f82a83cf3b5199ff112
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-19 00:15:08 +0530

    Initial Commit

---
+++ Arrays/ArraySumWithCondition.java
@@ -0,0 +1,24 @@
+public class ArraySumWithCondition {
+    public static void main(String[] args) {
+        int[] dataValues = { 10, 3, 6, 1, 2, 7, 9 };
+
+        int totalValue = 0;
+        boolean skipFlag = false;
+
+        for (int currentElement : dataValues) {
+            if (currentElement == 6) {
+                skipFlag = true;
+                continue;
+            }
+            if (currentElement == 7 && skipFlag) {
+                skipFlag = false;
+                continue;
+            }
+            if (!skipFlag) {
+                totalValue += currentElement;
+            }
+        }
+
+        System.out.println("The sum of array elements is: " + totalValue);
+    }
+}
