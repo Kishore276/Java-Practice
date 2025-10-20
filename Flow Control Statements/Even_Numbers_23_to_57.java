5f3a9aa623c18dd39efdc8024d326839091a7868
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-20 20:50:00 +0530

    Initial Commit

---
+++ Flow Control Statements/Even_Numbers_23_to_57.java
@@ -0,0 +1,11 @@
+public class Even_Numbers_23_to_57 {
+    public static void main(String[] args) {
+        System.out.println("Even numbers between 23 and 57 are:");
+
+        for (int number = 23; number <= 57; number++) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+    }
+}
