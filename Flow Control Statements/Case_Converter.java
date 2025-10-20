3b3f10c9b4725bc211ff8ec34799497973112adb
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-20 20:49:02 +0530

    Initial Commit

---
+++ Flow Control Statements/Case_Converter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Case_Converter {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a single alphabet character: ");
+        char inputChar = reader.next().charAt(0);
+
+        if (inputChar >= 'a' && inputChar <= 'z') {
+            System.out.println(inputChar + " -> " + Character.toUpperCase(inputChar));
+        } else if (inputChar >= 'A' && inputChar <= 'Z') {
+            System.out.println(inputChar + " -> " + Character.toLowerCase(inputChar));
+        } else {
+            System.out.println("Invalid input! Please enter an alphabet character.");
+        }
+
+        reader.close();
+    }
+}
