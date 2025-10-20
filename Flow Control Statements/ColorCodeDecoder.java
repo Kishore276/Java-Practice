426b7cf7d5f2c2a5459ad88902c2596b37ce93d8
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-20 20:49:36 +0530

    Initial Commit

---
+++ Flow Control Statements/ColorCodeDecoder.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ColorCodeDecoder {
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+
+        System.out.print("Enter a color code (R, B, G, O, Y, W): ");
+        char colorCode = inputReader.next().toUpperCase().charAt(0);
+
+        switch (colorCode) {
+            case 'R':
+                System.out.println("Red");
+                break;
+            case 'B':
+                System.out.println("Blue");
+                break;
+            case 'G':
+                System.out.println("Green");
+                break;
+            case 'O':
+                System.out.println("Orange");
+                break;
+            case 'Y':
+                System.out.println("Yellow");
+                break;
+            case 'W':
+                System.out.println("White");
+                break;
+            default:
+                System.out.println("Invalid Code");
+        }
+
+        inputReader.close();
+    }
+}
