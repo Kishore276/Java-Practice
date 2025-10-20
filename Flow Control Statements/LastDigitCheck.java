16b028f1e45c39fd766b5c050100c7a92a7c6efb
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-20 20:51:27 +0530

    Initial Commit

---
+++ Flow Control Statements/LastDigitCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LastDigitCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first non-negative integer: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Enter second non-negative integer: ");
+        int secondNumber = scanner.nextInt();
+
+        int firstDigit = firstNumber % 10;
+        int secondDigit = secondNumber % 10;
+
+        boolean isSame = (firstDigit == secondDigit);
+
+        System.out.println("Do " + firstNumber + " and " + secondNumber + " have the same last digit? " + isSame);
+
+        scanner.close();
+    }
+}
