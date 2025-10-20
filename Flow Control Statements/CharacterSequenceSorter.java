c8cd9061535216d7e06ea81e9c487c0e82b3c380
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-20 20:49:09 +0530

    Initial Commit

---
+++ Flow Control Statements/CharacterSequenceSorter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CharacterSequenceSorter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first character: ");
+        char firstChar = input.next().charAt(0);
+        System.out.print("Enter second character: ");
+        char secondChar = input.next().charAt(0);
+
+        if (firstChar < secondChar) {
+            System.out.println(firstChar + "," + secondChar);
+        } else {
+            System.out.println(secondChar + "," + firstChar);
+        }
+
+        input.close();
+    }
+}
