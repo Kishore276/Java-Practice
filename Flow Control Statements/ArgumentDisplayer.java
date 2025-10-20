b33e22b16b4ba9e8d40cffda701025d3b554a029
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-20 20:48:54 +0530

    Initial Commit

---
+++ Flow Control Statements/ArgumentDisplayer.java
@@ -0,0 +1,14 @@
+public class ArgumentDisplayer {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No values");
+        } else {
+            for (int index = 0; index < args.length; index++) {
+                System.out.print(args[index]);
+                if (index < args.length - 1) {
+                    System.out.print(",");
+                }
+            }
+        }
+    }
+}
