621626dd3074268213bd708cbbface30fe5d7412
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-22 04:42:42 +0530

    Initial Commit

---
+++ Language Basics/WelcomeMessageGenerator.java
@@ -0,0 +1,9 @@
+public class WelcomeMessageGenerator {
+    public static void main(String[] commandLineArgs) {
+        if (commandLineArgs.length == 0) {
+            System.out.println("Please provide a name as a command line argument.");
+        } else {
+            System.out.println("Welcome " + commandLineArgs[0]);
+        }
+    }
+}
