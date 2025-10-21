40004a44865d89773c12d3704dbe3387067f3062
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-21 17:45:02 +0530

    Initial Commit

---
+++ Flow Control Statements/SequentialNumberPrinter.java
@@ -0,0 +1,7 @@
+public class SequentialNumberPrinter {
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.print(counter + "\t");
+        }
+    }
+}
