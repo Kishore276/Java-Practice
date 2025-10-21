921d56b46b094737c8e7d49c600e68bd587564e3
G.YUVA KISHORE REDDY <g.yuvakishorereddy@gmail.com>
2025-10-22 04:41:51 +0530

    Initial Commit

---
+++ Language Basics/IntegerSummationTool.java
@@ -0,0 +1,12 @@
+public class IntegerSummationTool {
+    public static void main(String[] inputArgs) {
+        if (inputArgs.length != 2) {
+            System.out.println("Please provide exactly two integers as arguments.");
+            return;
+        }
+        int firstNumber = Integer.parseInt(inputArgs[0]);
+        int secondNumber = Integer.parseInt(inputArgs[1]);
+        int totalSum = firstNumber + secondNumber;
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is " + totalSum);
+    }
+}
